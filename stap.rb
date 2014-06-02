117d3224be4708a86b2ae90a1f6f692ec61f3b44
ぶっちゃん！ <g.dark.matter@gmail.com>
2014-06-03 00:09:34 +0900

    Create stap.rb

---
+++ stap.rb
@@ -0,0 +1,12 @@
+# coding: utf-8
+n = 0
+while loop
+	foo = (("A".."Z").to_a).sample(4).join
+	print foo + "細胞"
+	n = n + 1
+	if foo == "STAP"
+		print "\n", n, "'回目でSTAP細胞は見つかりました☆"
+		break
+	end
+end
+
